929a3e242a90c88f845b4f82370f42ce49f2d304
jayathungek <jayathunge.work@gmail.com>
2019-12-13 17:18:01 +0400

    remove comments

--- app.py
+++ app.py
@@ -1,13 +1,13 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify 
 from lsystem import LSystem 
-import json
+import json, os
 app = Flask(__name__)
 
 @app.route('/', methods=['GET'])
-def verify():
+def verify(): 
     # when the endpoint is registered as a webhook, it must echo back
     # the 'hub.challenge' value it receives in the query arguments
-    if request.args.get("hub.mode") == "subscribe" and request.args.get("hub.challenge"):
+    if request.args.get("hub.mode") == "subscribe" and request.args.get("hub.challenge"): 
         if not request.args.get("hub.verify_token") == os.environ["VERIFY_TOKEN"]:
             return "Verification token mismatch", 403
         return request.args["hub.challenge"], 200
