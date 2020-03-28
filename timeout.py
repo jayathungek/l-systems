4872bb4fa54aa9494aaf140ffc5d6fff4bb171b5
jayathungek <jayathunge.work@gmail.com>
2020-03-28 23:37:42 +0400

    add second argument to decorator

--- timeout.py
+++ timeout.py
@@ -6,7 +6,7 @@ import signal
 import params
 
 def timeout(func, seconds=params.TIMEOUT):
-    def decorator(decarg): 
+    def decorator(decarg1, decarg2): 
         def _handle_timeout(signum, frame):
             print("timeout triggered")
             raise error.ResponseTimeoutError()
@@ -16,7 +16,7 @@ def timeout(func, seconds=params.TIMEOUT):
             signal.alarm(seconds)
             try:
                 print("starting image creation")
-                result = func(decarg)
+                result = func(decarg1, decarg2)
             finally:
                 signal.alarm(0)
             return result
